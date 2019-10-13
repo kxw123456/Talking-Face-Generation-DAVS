b4e94e3a710dc2be85e43b974a54c82f463e0bd8
kxw123456 <40381158+kxw123456@users.noreply.github.com>
2019-10-13 15:53:52 +0800

    Update face_align.py

--- preprocess/face_align.py
+++ preprocess/face_align.py
@@ -11,9 +11,19 @@ import scipy.io as scio
 
 
 def transformation_from_points(points1, scale):
+    '''
+    arg：
+        points1:   (3,2)
+            3个点：左眼、右眼、嘴中央
+        scale：
+            2
+    return：
+        
+          
+    '''
     points = [[70, 112],
               [110, 112],
-              [90,  150]]
+              [90,  150]]       # 维度：(3,2) 
     points2 = np.array(points) * scale
     points2 = points2.astype(np.float64)
     points1 = points1.astype(np.float64)
@@ -28,22 +38,22 @@ def transformation_from_points(points1, scale):
     points1 /= s1
     points2 /= s2
 
-    U, S, Vt = np.linalg.svd(np.matmul(points1.T, points2))
+    U, S, Vt = np.linalg.svd(np.matmul(points1.T, points2))     # (2,3) * (3,2) --> (2,2)    奇异值分解
     R = (np.matmul(U, Vt)).T
     sR = (s2 / s1) * R
     T = c2.reshape(2,1) - (s2 / s1) * np.matmul(R, c1.reshape(2,1))
     M = np.concatenate((sR, T), axis=1)
-    return M
+    return M      # 维度： （2,3）
 
 
 class ImageLoader(object):
     def __init__(self, mode='test'):
         self.scale = 2
-        self.crop_height = 134 * self.scale
-        self.crop_width = 134 * self.scale
-        self.crop_center_y_offset = 10 * self.scale
+        self.crop_height = 134 * self.scale     # 134*2 = 268
+        self.crop_width = 134 * self.scale      # 134*2 = 268
+        self.crop_center_y_offset = 10 * self.scale     # 10 * 2 = 20
         self.output_scale = (260, 260)
-        self.ori_scale = (178 * self.scale, 218 * self.scale)
+        self.ori_scale = (178 * self.scale, 218 * self.scale)       # 178*2 = 356   218*2 = 436
         self.random_x = 0
         self.random_y = 0
         self.flip = 0
@@ -53,6 +63,11 @@ class ImageLoader(object):
             self.random_y = np.random.randint(-3, 4)
 
     def image_loader(self, path, points):
+        '''
+            仅处理一张图片
+            path: 图片的路径
+            points：点的列表， 至少包含 4个点， 分别为 左眼、右眼、两个嘴角
+        '''
         if os.path.exists(path):
             img = cv2.imread(path)
             three_points = np.zeros((3, 2))
@@ -62,12 +77,12 @@ class ImageLoader(object):
             three_points.astype(np.float32)
             M = transformation_from_points(three_points, self.scale)
             align_img = cv2.warpAffine(img, M, self.ori_scale, borderValue=[127, 127, 127])
-            l = int(round(self.ori_scale[0] / 2 - self.crop_width / 2 + self.random_x))
-            r = int(round(self.ori_scale[0] / 2 + self.crop_width / 2 + self.random_x))
-            t = int(round(self.ori_scale[1] / 2 - self.crop_height / 2 + self.crop_center_y_offset + self.random_y))
-            d = int(round(self.ori_scale[1] / 2 + self.crop_height / 2 + self.crop_center_y_offset + self.random_y))
+            l = int(round(self.ori_scale[0] / 2 - self.crop_width / 2 + self.random_x))     # 178 - 134 + x
+            r = int(round(self.ori_scale[0] / 2 + self.crop_width / 2 + self.random_x))     # 178 + 134 + x
+            t = int(round(self.ori_scale[1] / 2 - self.crop_height / 2 + self.crop_center_y_offset + self.random_y))    # 218-134+20 + y
+            d = int(round(self.ori_scale[1] / 2 + self.crop_height / 2 + self.crop_center_y_offset + self.random_y))    # 218+134+20 + y
             align_img2 = align_img[t:d, l:r, :]
-            align_img2 = cv2.resize(align_img2, self.output_scale)
+            align_img2 = cv2.resize(align_img2, self.output_scale)      # (260,260)
             return align_img2
         else:
             raise ("image = 0")
