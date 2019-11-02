5ee986e3e45b42d09e81df97cb74e6bce4d46e79
kxw123456 <40381158+kxw123456@users.noreply.github.com>
2019-11-02 22:17:04 +0800

    Update save_mfccs.m

--- preprocess/save_mfccs.m
+++ preprocess/save_mfccs.m
@@ -1,4 +1,4 @@
-mainPath = '/home/hzhou/data/LRW/new_data/';
+mainPath = '/home/luxb/kxw/DAVS/new_data/';
 audio = 'mfcc20';
 
 opt.fs = 16000;
@@ -13,45 +13,45 @@ opt.L = 22;
 csv_name = 'video_look_up_table.csv';
 p_name = {'test', 'train','val'};
 for p = 3
-    c = p_name(p);
-    pathc = fullfile(mainPath, c);
+    c = p_name(p);    
+    pathc = fullfile(mainPath, c);      % '~/val'
     for word = 1 : 500
         w = word - 1;
-        pathw = fullfile(pathc, num2str(w));
-        pathw = pathw{1};
-        csv_path = fullfile(pathw, csv_name);
-        M = readtable(char(csv_path));
+        pathw = fullfile(pathc, num2str(w));   % '~/val/0'
+        pathw = pathw{1};               % ~/val/0
+        csv_path = fullfile(pathw, csv_name);  % ~/val/0/vid/video_look_up_table.csv
+        M = readtable(char(csv_path));  
         [num_files, b] = size(M);
         for k = 0:num_files-1
-            file_path = fullfile(pathw, num2str(k));
-            video_path = fullfile(file_path, [num2str(k), '.wav']);
-            if exist(fullfile(file_path, 'mfcc.bin'), 'file')
+            file_path = fullfile(pathw, num2str(k));    % ~/val/0/0
+            video_path = fullfile(file_path, [num2str(k), '.wav']);     % ~/val/0/0/0.wav
+            if exist(fullfile(file_path, 'mfcc.bin'), 'file')           % ~/val//0/0/mfcc.bin
                 f = fopen(fullfile(file_path, 'mfcc.bin'), 'r');
                 A = fread(f, 'double');
                 mfccs = reshape(A, 13, 122);
                 mfccs = mfccs(2:end, :);
                 for l = 2:26
                     save_mfcc20 = mfccs(:, 4 * l -7  : 4 * l + 19 -7);
-                    mfcc_path = fullfile(file_path, audio);
+                    mfcc_path = fullfile(file_path, audio);             % ~/val/0/0/mfcc20
                     if ~exist(mfcc_path, 'dir')
                         mkdir(mfcc_path)
                     end
-                    f2 = fopen(fullfile(mfcc_path, [num2str(l), '.bin']), 'wb');
+                    f2 = fopen(fullfile(mfcc_path, [num2str(l), '.bin']), 'wb');     % ~/val/0/0/mfcc20/2.bin
                     fwrite(f2, save_mfcc20, 'double');
                     fclose(f2);                    
                 end
             else
-                [Speech, fs] = audioread(video_path);
+                [Speech, fs] = audioread(video_path);           % ~/val/0/0/0.wav
                 Speech = Speech(1: end - 600);
                 [ MFCCs, FBE, frames ] = runmfcc( Speech, opt );
-                f = fopen(fullfile(file_path, 'mfcc.bin'), 'wb');
+                f = fopen(fullfile(file_path, 'mfcc.bin'), 'wb');   % ~/val/0/0/mfcc.bin
                 fwrite(f, MFCCs, 'double');
                 fclose(f);
                 
                 mfccs = MFCCs(2:end, :);
                 for l = 2:26
                     save_mfcc20 = mfccs(:, 4 * l -7  : 4 * l + 19 -7);
-                    mfcc_path = fullfile(file_path, audio);
+                    mfcc_path = fullfile(file_path, audio);         % ~/val/0/0/mfcc20
                     if ~exist(mfcc_path, 'dir')
                         mkdir(mfcc_path)
                     end
